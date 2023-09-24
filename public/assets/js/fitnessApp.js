ac9149b9cce4b74e40eb403272678bf99e24a5bf
Ricky Barfield <barfieldfredrick@gmail.com>
2023-09-24 06:44:13 -0700

    heavily edited front page, made conditionals to eliminate risky quotes, beginning to toggle sign in/register displays

--- public/assets/js/fitnessApp.js
+++ public/assets/js/fitnessApp.js
@@ -1,7 +1,14 @@
+// header quote generation
 async function quoteApiRequest() {
     try {
-        const res = await fetch('https://api.kanye.rest')
-        const data = await res.json()
+        let res = await fetch('https://api.kanye.rest')
+        let data = await res.json()
+
+        // runs API fetch again if risky quotes are given.
+        if (data.quote === 'I feel like me and Taylor might still have sex' || data.quote === 'Shut the fuck up I will fucking laser you with alien fucking eyes and explode your fucking head' ) {
+            res = await fetch('https://api.kanye.rest')
+            data = await res.json()
+        }
 
         document.getElementById('quote').innerText = `Randomized motivational Kanye West quote of the session: \n '${data.quote}'`
     } catch (error) {
@@ -9,5 +16,7 @@ async function quoteApiRequest() {
     }
 
 }
+
+
 
 quoteApiRequest()
