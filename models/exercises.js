5c112b1ccc2a262192fd2fcd2db8fe224da46044
Ricky Barfield <barfieldfredrick@gmail.com>
2023-09-26 15:16:06 -0700

    added more Schema rules

--- models/exercises.js
+++ models/exercises.js
@@ -4,7 +4,19 @@ const ExerciseSchema = new mongoose.Schema({
     exercise: {
         type: String,
         required: true,
-    }
+    },
+    sets: {
+        type: Number,
+        required: true,
+    },
+    reps: {
+        type: Number,
+        required: true,
+    },
+    timer: {
+        type: Boolean,
+        required: false,
+    }, 
 })
 
 module.exports = mongoose.model('Exercise', ExerciseSchema)
