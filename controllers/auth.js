58daa4d56a7c087343d95eb9f36325da1ab669ad
Ricky Barfield <barfieldfredrick@gmail.com>
2023-09-27 13:00:32 -0700

    getting close to getting login operational...But made a workout button until I figure it out.

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,30 +11,34 @@ const User = require('../models/User')
     })
   }
   
-  exports.postLogin = (req, res, next) => {
-    const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
-  
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors)
-      return res.redirect('/login')
-    }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
-  
-    passport.authenticate('local', (err, user, info) => {
-      if (err) { return next(err) }
-      if (!user) {
-        req.flash('errors', info)
-        return res.redirect('/login')
+  exports.postLogin = async (req, res, next) => {
+    try {
+        const validationErrors = []
+      if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
+      if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
+    
+      if (validationErrors.length) {
+        req.flash('errors', validationErrors)
+        return res.redirect('/')
       }
-      req.logIn(user, (err) => {
+      req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+    
+      passport.authenticate('local', (err, user, info) => {
         if (err) { return next(err) }
-        req.flash('success', { msg: 'Success! You are logged in.' })
-        res.redirect(req.session.returnTo || '/fitnessApp')
-      })
-    })(req, res, next)
+        if (!user) {
+          req.flash('errors', info)
+          return res.redirect('/')
+        }
+        req.logIn(user, (err) => {
+          if (err) { return next(err) }
+          req.flash('success', { msg: 'Success! You are logged in.' })
+          res.redirect(req.session.returnTo || '/fitnessApp')
+        })
+      })(req, res, next)
+  } catch (err) {
+    return next(err)
   }
+}
   
   exports.logout = (req, res) => {
     req.logout(() => {
@@ -77,9 +81,9 @@ const User = require('../models/User')
         gmail_remove_dots: false
       });
   
-      const existingUser = await User.findOne({
-        $or: [{ email: req.body.email }, { userName: req.body.userName }]
-      });
+      // const existingUser = await User.findOne({
+      //   $or: [{ email: req.body.email }, { userName: req.body.userName }]
+      // });
   
       if (existingUser) {
         req.flash("errors", {
@@ -108,7 +112,7 @@ const User = require('../models/User')
   };
 
   // Dated code - no longer works for postSignUp
-  
+
   // exports.postSignup = (req, res, next) => {
   //   const validationErrors = []
   //   if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
