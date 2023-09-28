71cd7c1348177c5b133243123192a6ddbafd5f79
Ricky Barfield <barfieldfredrick@gmail.com>
2023-09-27 21:46:34 -0700

    more styling

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,7 +8,10 @@
 
 //workout tracking:
 // reps, are trackable via button click/text field gathering. Timer can be applied to others when applicable. OR perhaps a timer function could just be included for all workouts but toggleable as you can make anything have a isometric component.
-//* User should be able to add their own workouts as well as delete them via trashcan icon
+// User should be able to add their own workouts as well as delete them via trashcan icon
+// User should be able to add notes to each individual exercise to track any extra feelings they have about the work that they did
+
+// Maybe In the custom exercise bank - the user's exercises will be presented first, but the entire community's exercises are also accessable. They can even add their own descriptions and gifs of how to do said exercises on their own....This list should definitely be searchable though by title and maybe eventually musclegroup category.
 
 (function($) {
 
