7594140967cca1d361812c72ffa8a6f8dc2ec3b2
Ricky Barfield <barfieldfredrick@gmail.com>
2023-10-08 08:49:11 -0700

    gutting fitness app template because it was hard to work with. Beginning to change increment buttons to  numerical form submission for improving user experience

--- public/assets/js/fitnessApp.js
+++ public/assets/js/fitnessApp.js
@@ -22,7 +22,7 @@ async function quoteApiRequest() {
             data = await res.json()
         }
 
-        document.getElementById('quote').innerText = `Randomized motivational Kanye West quote of the session: \n '${data.quote}'`
+        document.getElementById('quote').innerText = `Randomized motivational Kanye West quote of the session: \n \n '${data.quote}'`
     } catch (error) {
         console.log(error)
     }
